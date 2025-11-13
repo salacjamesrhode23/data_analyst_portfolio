2b7776006c50f8314584af2d2fc8478412d0fda2
salacjamesrhode77 <salac.jamesrhode77@gmail.com>
2025-11-13 10:00:17 +0000

    dbt etc. added

---
+++ ecommerce_data_engineering_pipeline/ecomm_dbt/target/run/ecomm_dbt/models/staging/stg_customers.sql
@@ -0,0 +1,18 @@
+
+
+  create or replace view `de-project-nyc-taxi`.`ecomm_staging`.`stg_customers`
+  OPTIONS()
+  as 
+
+SELECT
+  CAST(`Address City` AS STRING) AS city,
+  CAST(`Address Company` AS STRING) AS company,
+  CAST(`Address Province` AS STRING) AS province,
+  CAST(`Address Zip` AS STRING) AS zip,
+  CAST(Email AS STRING) AS email,
+  CAST(`First Name` AS STRING) AS first_name,
+  CAST(`Full Name` AS STRING) AS full_name,
+  CAST(`Last Name` AS STRING) AS last_name,
+  CAST(Phone AS STRING) AS phone
+FROM `de-project-nyc-taxi`.`staging`.`stg_api_customers`;
+
